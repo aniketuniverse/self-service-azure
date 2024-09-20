363012da7ea31c738bd8297cbf3fdc29c0dd1058
Aniket Mukherjee <mukherjee.aniket@gmail.com>
2024-09-20 22:20:48 +0100

    Update create-cluster.sh

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -6,4 +6,4 @@ FILE_PATH=infra/${NAME}-rg.yaml
 
 cp crossplane/rg-template.yaml $FILE_PATH
 yq --inplace ".metadata.name = \"${NAME}\"" $FILE_PATH
-yq --inplace ".spec.location = \"${LOCATION}\"" $FILE_PATH
+yq --inplace ".spec.forProvider.location = \"${LOCATION}\"" $FILE_PATH
